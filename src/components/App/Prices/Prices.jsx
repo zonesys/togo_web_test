a2c0736a419995d60b28d5e6ca4b52d0c263de5e
zonesys <sys@zone.ps>
2024-10-30 08:54:13 +0200

    ..

--- src/components/App/Prices/Prices.jsx
+++ src/components/App/Prices/Prices.jsx
@@ -3,10 +3,14 @@ import React from 'react';
 
 const Prices = () => {
   return (
+    <>
+    Hi
     <iframe
       src={"https://api.dev.togo.ps/prices"}
       sandbox="allow-scripts allow-same-origin allow-popups"
     />
+    </>
+    
   );
 };
 
