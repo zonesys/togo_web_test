deaa5b5b619918918ee7759b2aea6f3ddbe46635
zonesys <sys@zone.ps>
2024-11-13 11:55:15 +0200

    change .env

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -21,6 +21,7 @@ import PaymentRequest from "../PaymentPage/PaymentRedirect";
 import InitializeTransaction from "../PaymentPage/PaymentRedirect";
 import PaymentPage2 from "../PaymentPage/test";
 import Prices from "../AdminPanel/Prices/Prices";
+import 'dotenv/config'
 
 const App = () => {
 
@@ -28,7 +29,6 @@ const App = () => {
 
     const language = useSelector(state => state.general.language);
     localStorage.setItem("lang",language);
-
     /* ----------------------( FCM )---------------------- */
 
     // notReload used in order to update the DOM at every notification
