8b94ee38a6255d12dffcefeff0ce808f7a6f3194
zonesys <sys@zone.ps>
2024-11-13 10:59:46 +0200

    check if receiver name or phone exists

--- src/Constants/GeneralCont.js
+++ src/Constants/GeneralCont.js
@@ -1,4 +1,4 @@
-export const apiUrl = `https://${process.env.REACT_APP_DOMAIN}/togo/MobileAPi/public/FunctionApis.php`;
+export const apiUrl = `http://localhost:8001/MobileAPi/public/FunctionApis.php`;
 export const imgBaseUrl = `https://${process.env.REACT_APP_DOMAIN}/togo/MobileAPi/`;
 export const requestHeaders = {
     "Access-Control-Allow-Origin": `*`,
